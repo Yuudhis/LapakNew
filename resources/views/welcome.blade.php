7397d41ea9e02af872fa8fc4726985b19a3197c7
Yuudhis <55727057+Yuudhis@users.noreply.github.com>
2023-03-22 03:57:45 +0700

    hero

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -19,8 +19,8 @@
 
     {{-- <h1 class="flex justify-center text-6xl font-bold my-20">Hello World</h1> --}}
 
-
-    <nav class="sticky top-0 z-50 bg-white border-gray-200 mt-5 mx-20 sm:px-4 py-2.5 rounded dark:bg-gray-900">
+    {{-- navbar section --}}
+    <nav class="sticky top-0 z-50 bg-white mt-5 mx-20 sm:px-4 py-2.5 rounded dark:bg-gray-900">
 
         <div class="container flex flex-wrap items-center justify-between">
 
@@ -78,6 +78,42 @@
 
         </div>
     </nav>
+
+    {{-- hero section --}}
+    <section class="">
+
+        <div class="mx-auto max-w-screen-xl py-20 px-4 lg:flex lg:items-center">
+
+            <div class="mx-auto max-w-xl text-center">
+
+                <h1 class="text-3xl font-extrabold sm:text-5xl">
+                    Feel The Future In Your Hands With
+                    <strong class="mt-5 text-6xl font-extrabold text-transparent  bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600 sm:block">
+                        ChatGPT 3.5 Plus & ChatGPT 4
+                    </strong>
+                </h1>
+
+                <p class="mt-4 sm:text-xl sm:leading-relaxed">
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt illo
+                    tenetur fuga ducimus numquam ea!
+                </p>
+
+                <div class="mt-8 flex flex-wrap justify-center gap-4">
+                    <a class="block w-full rounded bg-pink-500 px-12 py-3 text-sm font-medium text-white shadow hover:bg-pink-600 focus:outline-none focus:ring active:bg-pink-400 sm:w-auto"
+                        href="/get-started">
+                        Get Started
+                    </a>
+
+                    <a class="block w-full rounded px-12 py-3 text-sm font-medium text-pink-500 shadow hover:text-pink-600 focus:outline-none focus:ring active:text-pink-400 sm:w-auto"
+                        href="/about">
+                        Learn More
+                    </a>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
 
     {{-- flowbite js script --}}
     <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.4/flowbite.min.js"></script>
