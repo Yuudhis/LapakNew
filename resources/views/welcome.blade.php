d256f32dafa9e82cd19d30217e60643f43cf2974
Yuudhis <55727057+Yuudhis@users.noreply.github.com>
2023-03-21 12:51:51 +0700

    first

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    {{-- tailwindcss --}}
+    @vite('resources/css/app.css')
+    <title>Document</title>
+</head>
+
+<body>
+
+    <h1 class="flex justify-center text-6xl font-bold my-20">Hello World</h1>
+
+</body>
+
+</html>
