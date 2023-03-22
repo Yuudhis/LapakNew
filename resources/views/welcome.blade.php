9e5b063e59f9016e080f45932bd29029e48ddcc5
Yuudhis <55727057+Yuudhis@users.noreply.github.com>
2023-03-22 17:33:45 +0700

    fixing price section

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -203,7 +203,8 @@
             class="inline-flex justify-center mb-5 text-6xl font-extrabold text-transparent  bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">
             Our Price</p>
 
-        <p class="inline-flex justify-center font-light text-gray-500">Dont worry about the price, we keep it low just
+        <p class="inline-flex justify-center font-light text-lg text-gray-500">Dont worry about the price, we keep it
+            low just
             for you so you can get creative!</p>
 
         <div class="flex justify-center gap-10 mt-10">
@@ -219,7 +220,8 @@
                 <div class="px-5 pb-5">
 
                     <a href="#">
-                        <h5 class="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">ChatGPT 3.5 Plus & <br> ChatGPT 4</h5>
+                        <h5 class="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">ChatGPT 3.5 Plus
+                            & <br> ChatGPT 4</h5>
                     </a>
 
                     <div class="flex items-center mt-2.5 mb-5">
@@ -259,13 +261,14 @@
                             </path>
                         </svg>
                         <span
-                            class="bg-blue-100 text-blue-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ml-3">5.0</span>
+                            class="bg-red-100 text-red-500 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded ml-3">HOT</span>
                     </div>
 
                     <div class="flex items-center justify-between">
                         <span class="text-3xl font-bold text-gray-900 dark:text-white">Rp 85.000</span>
-                        <a href="#"
-                            class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"> Subscribe</a>
+                        <a href=""
+                            class="text-white bg-gradient-to-r from-purple-400 to-pink-600 hover:bg-gradient-to-r hover:from-purple-600 hover:to-pink-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center">
+                            Subscribe</a>
                     </div>
                 </div>
             </div>
